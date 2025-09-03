58b234a258613c530fe287abf4345c02efcb17af
Jaime Martins <contato.jaimemartins@gmail.com>
2025-09-03 10:19:57 -0300

    Remove unused `modal_callback` route and related FastAPI dependencies

--- back-end/app/modal_callback.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-
-router = APIRouter()
-
-@router.post("/modal-callback")
-async def modal_callback(request: Request):
-    data = await request.json()
-    print("Modal result:", data)
-    return {"status": "received"}
