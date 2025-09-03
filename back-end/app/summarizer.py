8b7057fc541079899ec8e371e1523406884ae06a
Jaime Martins <contato.jaimemartins@gmail.com>
2025-09-03 10:20:49 -0300

    Remove

--- back-end/app/summarizer.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def summarize_text(text: str, max_length: int = 150) -> str:
-    summary = summarizer(text, max_length=max_length, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
