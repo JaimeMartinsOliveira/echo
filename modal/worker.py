42d84da21f667dfa3c9022e3984255ae268a2d12
Jaime Martins <contato.jaimemartins@gmail.com>
2025-09-03 10:21:00 -0300

    Remove

--- modal/worker.py
+++
@@ -1,7 +0,0 @@
-import whisperx
-
-def run_worker(file_path: str):
-    model = whisperx.load_model("small", device="cpu")
-    audio = whisperx.load_audio(file_path)
-    result = model.transcribe(audio)
-    return result
