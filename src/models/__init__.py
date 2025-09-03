8adcd28c66f814ada54e842d7eb58ab23c01a75a
Jaime Martins <contato.jaimemartins@gmail.com>
2025-09-03 10:42:04 -0300

    Export

---
+++ src/models/__init__.py
@@ -0,0 +1,10 @@
+from .transcription import TranscriptionRequest, TranscriptionResponse, TranscriptionResult, TranscriptionStatus
+from .job import Job
+
+__all__ = [
+    "TranscriptionRequest",
+    "TranscriptionResponse",
+    "TranscriptionResult",
+    "TranscriptionStatus",
+    "Job"
+]
