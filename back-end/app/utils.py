2da9838b61f8e0631fd53e598903639783b866d7
Jaime Martins <contato.jaimemartins@gmail.com>
2025-09-03 10:20:56 -0300

    Remove

--- back-end/app/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-def get_env_var(name: str, default=None):
-    return os.getenv(name, default)
-
-def ensure_dir(path: str):
-    Path(path).mkdir(parents=True, exist_ok=True)
